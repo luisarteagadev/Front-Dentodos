2e0861fe882fe801e761ae711c0b70c8faae84ea
luisarteagadev <l.angelarteaga713@gmail.com>
2025-06-21 16:13:58 -0500

    Se hace responsive el Footer

--- src/components/EndFooter.vue
+++ src/components/EndFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer>
+  <!-- <footer>
     <div id="main"><img src="/media/logos/logo_circle.png" alt="" /></div>
     <div id="main">
       <p>Contacto</p>
@@ -32,6 +32,54 @@
       </div>
     </div>
     <div id="copy"><center>&copy; Dentodos 2025</center></div>
+  </footer> -->
+  <footer id="footer">
+    <div class="container-footer">
+      <div class="footer-row">
+        <div class="footer-col">
+          <div id="main"><img src="/media/logos/logo_circle.png" alt="" /></div>
+        </div>
+        <div class="footer-col">
+          <h4>Contacto</h4>
+          <ul class="links">
+            <li><a>jirón Caracas 2527 Jesús Maria </a></li>
+            <li><a>dentodos.adm@gmail.com </a></li>
+            <li><a>+51 932 033 644 </a></li>
+          </ul>
+        </div>
+
+        <div class="footer-col">
+          <h4>Horarios</h4>
+          <ul class="links">
+            <li><a>Lunes a Sábados </a></li>
+            <li><a>10:00 am - 7:00 pm </a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>Siguenos</h4>
+
+          <ul class="links">
+            <div class="social-icons">
+              <a
+                href="https://www.instagram.com/dentodos.peru?utm_source=qr&igsh=MTFubnd6anM5dnVncw=="
+                target="_blank"
+                ><img src="/media/icons/instagram.png" alt="Instagram"
+              /></a>
+              <a href="https://www.facebook.com/share/1SidkLWFxN/" target="_blank"
+                ><img src="/media/icons/facebook.png" alt="Facebook"
+              /></a>
+              <a href="https://www.tiktok.com/@dentodos.peru?_t=ZM-8wW8CswOLuq&_r=1" target="_blank"
+                ><img src="/media/icons/tiktok.png" alt="Tiktok"
+              /></a>
+              <a href="https://youtube.com/@dentodosperu9701?si=NaAy9eBRfA7CiwUI" target="_blank"
+                ><img src="/media/icons/youtube.png" alt="YouTube"
+              /></a>
+            </div>
+          </ul>
+        </div>
+      </div>
+      <div id="copy"><center>&copy; Dentodos 2025</center></div>
+    </div>
   </footer>
 </template>
 
